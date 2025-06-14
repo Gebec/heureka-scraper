9b915be0e082d2e7ef4e031b50817489cb867140
gebec <gebec.m@gmail.com>
2025-06-14 22:34:36 +0200

    fix: minor fixes

--- src/scraper.ts
+++ src/scraper.ts
@@ -4,8 +4,8 @@ import { savePriceToDb, initDb } from './db.js';
 import { loadList } from './loadList.js';
 
 (async () => {
-  await initDb();
-  const list = await loadList();
+  const [list] = await Promise.all([loadList(), initDb()]);
+  const rowsCount = list.length;
 
   const browser = await chromium.launch({
     headless: false,
@@ -20,62 +20,67 @@ import { loadList } from './loadList.js';
 
   const page = await browser.newPage();
 
-  for (const lego of list) {
+  for (const [index, row] of list.entries()) {
     const cookies = JSON.parse(fs.readFileSync('cookies.json').toString());
     await context.addCookies(cookies);
 
-    console.log(`Scraping: ${lego.url}`);
-    await page.goto(lego.url, { waitUntil: 'domcontentloaded' });
+    try {
+      console.log(`${index + 1}/${rowsCount}: ${row.url}`);
+      await page.goto(row.url, { waitUntil: 'domcontentloaded' });
 
-    const containers = await page.$$('[data-testid="Offer List Container"]');
-    if (containers.length === 0) {
-      console.warn(`No 'Offer List Container' found for url: ${lego.url}`);
+      const containers = await page.$$('[data-testid="Offer List Container"]');
+      if (containers.length === 0) {
+        console.warn(`No 'Offer List Container' found for url: ${row.url}`);
+        continue;
+      }
+
+      const targetContainer = containers.length > 1 ? containers.at(1) : containers.at(0);
+      if (!targetContainer) {
+        console.warn(`No valid container found for url: ${row.url}`);
+        continue;
+      }
+
+      const results = await targetContainer.$$eval('div.c-offer__inner', (nodes) => {
+        const getPrice = (item: HTMLElement): number | null => {
+          const priceElement = item.querySelector('span.c-offer__price');
+          const text = priceElement?.textContent;
+          if (!text) return null;
+
+          const price = parseInt(text.replace(/[^\d]/g, ''));
+          return isNaN(price) ? null : price;
+        };
+
+        const getShopName = (item: HTMLElement): string | null => {
+          const priceElement = item.querySelector('img.c-offer__shop-logo');
+          const alt = priceElement?.getAttribute('alt');
+          if (!alt) return null;
+
+          return alt.replace(/logo\s*/i, '').trim();
+        };
+
+        return (nodes as HTMLElement[]).slice(0, 5).map((item, index) => {
+          const price = getPrice(item);
+          const shopName = getShopName(item);
+          const position = index + 1;
+
+          return { price, shopName, position };
+        });
+      });
+
+      results.forEach(({ position, price, shopName }) => {
+        savePriceToDb({
+          legoId: row.id,
+          position,
+          price,
+          shopName,
+        });
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    } catch (error) {
+      console.error(`Error processing row ${index + 1}/${rowsCount}:`, error);
       continue;
     }
-
-    const targetContainer = containers.length > 1 ? containers.at(1) : containers.at(0);
-    if (!targetContainer) {
-      console.warn(`No valid container found for url: ${lego.url}`);
-      continue;
-    }
-
-    const results = await targetContainer.$$eval('div.c-offer__inner', (nodes) => {
-      const getPrice = (item: HTMLElement): number | null => {
-        const priceElement = item.querySelector('span.c-offer__price');
-        const text = priceElement?.textContent;
-        if (!text) return null;
-
-        const price = parseInt(text.replace(/[^\d]/g, ''));
-        return isNaN(price) ? null : price;
-      };
-
-      const getShopName = (item: HTMLElement): string | null => {
-        const priceElement = item.querySelector('img.c-offer__shop-logo');
-        const alt = priceElement?.getAttribute('alt');
-        if (!alt) return null;
-
-        return alt.replace(/logo\s*/i, '').trim();
-      };
-
-      return (nodes as HTMLElement[]).slice(0, 5).map((item, index) => {
-        const price = getPrice(item);
-        const shopName = getShopName(item);
-        const position = index + 1;
-
-        return { price, shopName, position };
-      });
-    });
-
-    results.forEach(({ position, price, shopName }) => {
-      savePriceToDb({
-        legoId: lego.id,
-        position,
-        price,
-        shopName,
-      });
-    });
-
-    await new Promise((resolve) => setTimeout(resolve, 2000));
   }
 
   await browser.close();
